a57c327ac71d2d33037e514a6798940bbe9be8b4
YKCash-828 <ycasher@pvamu.edu>
2025-03-11 08:07:57 -0500

    Updated the hello_world program in main

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,4 @@
-import random
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
 
-greetings = ["Hello", "Hi", "Hey", "Greetings", "Salutations"]
-name = input("Enter your name: ").strip() or "World"
-print(f"{random.choice(greetings)}, {name}!")
 
